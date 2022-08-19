f6c5d9fc6a682e25d1a87b37532b8b7152cd7cd3
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-08-18 23:15:43 -0600

    changes

--- app/src/main/java/com/jimena/pm_l6/MainActivity.kt
+++ app/src/main/java/com/jimena/pm_l6/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.content.Intent
+import android.widget.Button
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNav: BottomNavigationView
@@ -22,12 +26,13 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setOnItemSelectedListener {
             // Dependiendo el item del menu, mostramos el fragment deseado
             when(it.itemId) {
-                R.id.item_home -> setCurrentFragment(Home())
+                R.id.item_home -> setCurrentFragment(Home.newInstance("Jimena","Hernandez"))
                 R.id.item_search -> setCurrentFragment(Search())
                 R.id.item_library -> setCurrentFragment(Library())
             }
             true
         }
+
     }
 
     /**
