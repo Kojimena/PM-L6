fdaa9806b645e75956d38209c39969455e3f72d1
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2022-08-18 22:07:03 -0600

    firstcommit

--- app/src/main/java/com/jimena/pm_l6/MainActivity.kt
+++ app/src/main/java/com/jimena/pm_l6/MainActivity.kt
@@ -2,10 +2,42 @@ package com.jimena.pm_l6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNav: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.bottomNav_bottomNavActivity)
+        setCurrentFragment(Home())
+        setListeners()
+    }
+
+    private fun setListeners() {
+        bottomNav.setOnItemSelectedListener {
+            // Dependiendo el item del menu, mostramos el fragment deseado
+            when(it.itemId) {
+                R.id.item_home -> setCurrentFragment(Home())
+                R.id.item_search -> setCurrentFragment(Search())
+                R.id.item_library -> setCurrentFragment(Library())
+            }
+            true
+        }
+    }
+
+    /**
+     * Función que reemplaza el fragment actual. No usamos backstack porque
+     * para este tipo de navegación, no es el comportamiento esperado.
+     */
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(R.id.fragmentContainer_bottomNavActivity, fragment)
+        }
     }
 }
